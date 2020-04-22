0e6732e513dae7095a89fd7cd01a76312ca67b5f
Sylvain Prat <sylvain.prat@gmail.com>
2020-04-22 23:01:15 +0200

    Added a real test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -2,10 +2,18 @@ import os
 
 import testinfra.utils.ansible_runner
 
+here = os.path.dirname(__file__)
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']
 ).get_hosts('all')
 
 
-def test(host):
-    pass
+def read_reference_file():
+    with open(os.path.join(here, 'snapraid.reference.conf')) as reference_file:
+        return reference_file.read()
+
+
+def test_configuration_file(host):
+    reference = read_reference_file()
+    configuration = host.file("/etc/snapraid.conf").content_string
+    assert configuration == reference
